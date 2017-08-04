2633843d1e1da2b8d35f2e3696f1495136b12db0
Darren Eng <darrenle9@gmail.com>
2017-08-03 21:37:32 -0700

    remove postgres username dependency

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :tadpoll, Tadpoll.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  # username: "postgres",
+  # password: "postgres",
   database: "tadpoll_dev",
   hostname: "localhost",
   pool_size: 10
