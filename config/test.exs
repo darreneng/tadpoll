bc6d6776f3205026cba10e325d56fdb187e6bc2d
Darren Eng <darrenle9@gmail.com>
2017-08-06 22:25:28 -0700

    remove postgres username dependency for test config

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :tadpoll, Tadpoll.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  # username: "postgres",
+  # password: "postgres",
   database: "tadpoll_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
